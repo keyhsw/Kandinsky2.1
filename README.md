be60e58a41326e547c94b6fb4c6299da44a5037d
sberbank-ai <login-const@mail.ru>
2023-04-04 10:21:33 +0000

    Duplicate from AlexWortega/Kandinsky2.1
    Co-authored-by: Wortega <AlexWortega@users.noreply.huggingface.co>

---
+++ README.md
@@ -0,0 +1,13 @@
+---
+title: Kandinsky2.0
+emoji: 📉
+colorFrom: indigo
+colorTo: green
+sdk: gradio
+sdk_version: 3.11.0
+app_file: app.py
+pinned: false
+duplicated_from: AlexWortega/Kandinsky2.1
+---
+
+Check out the configuration reference at https://huggingface.co/docs/hub/spaces-config-reference
---
+++ app.py
@@ -0,0 +1,221 @@
+
+import gradio as gr
+import torch
+from torch import autocast
+from kandinsky2 import get_kandinsky2
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+from kandinsky2 import get_kandinsky2
+model = get_kandinsky2('cuda', task_type='text2img', model_version='2.1', use_flash_attention=False)
+
+
+
+def infer(prompt):
+    images = model.generate_text2img(prompt, num_steps=100,
+                          batch_size=1, guidance_scale=4,
+                           h=768, w=768,
+                           sampler='p_sampler', prior_cf_scale=4,
+                           prior_steps="5",)
+    return images
+
+css = """
+        .gradio-container {
+            font-family: 'IBM Plex Sans', sans-serif;
+        }
+        .gr-button {
+            color: white;
+            border-color: black;
+            background: black;
+        }
+        input[type='range'] {
+            accent-color: black;
+        }
+        .dark input[type='range'] {
+            accent-color: #dfdfdf;
+        }
+        .container {
+            max-width: 730px;
+            margin: auto;
+            padding-top: 1.5rem;
+        }
+        #gallery {
+            min-height: 22rem;
+            margin-bottom: 15px;
+            margin-left: auto;
+            margin-right: auto;
+            border-bottom-right-radius: .5rem !important;
+            border-bottom-left-radius: .5rem !important;
+        }
+        #gallery>div>.h-full {
+            min-height: 20rem;
+        }
+        .details:hover {
+            text-decoration: underline;
+        }
+        .gr-button {
+            white-space: nowrap;
+        }
+        .gr-button:focus {
+            border-color: rgb(147 197 253 / var(--tw-border-opacity));
+            outline: none;
+            box-shadow: var(--tw-ring-offset-shadow), var(--tw-ring-shadow), var(--tw-shadow, 0 0 #0000);
+            --tw-border-opacity: 1;
+            --tw-ring-offset-shadow: var(--tw-ring-inset) 0 0 0 var(--tw-ring-offset-width) var(--tw-ring-offset-color);
+            --tw-ring-shadow: var(--tw-ring-inset) 0 0 0 calc(3px var(--tw-ring-offset-width)) var(--tw-ring-color);
+            --tw-ring-color: rgb(191 219 254 / var(--tw-ring-opacity));
+            --tw-ring-opacity: .5;
+        }
+        #advanced-btn {
+            font-size: .7rem !important;
+            line-height: 19px;
+            margin-top: 12px;
+            margin-bottom: 12px;
+            padding: 2px 8px;
+            border-radius: 14px !important;
+        }
+        #advanced-options {
+            display: none;
+            margin-bottom: 20px;
+        }
+        .footer {
+            margin-bottom: 45px;
+            margin-top: 35px;
+            text-align: center;
+            border-bottom: 1px solid #e5e5e5;
+        }
+        .footer>p {
+            font-size: .8rem;
+            display: inline-block;
+            padding: 0 10px;
+            transform: translateY(10px);
+            background: white;
+        }
+        .dark .footer {
+            border-color: #303030;
+        }
+        .dark .footer>p {
+            background: #0b0f19;
+        }
+        .acknowledgments h4{
+            margin: 1.25em 0 .25em 0;
+            font-weight: bold;
+            font-size: 115%;
+        }
+        #container-advanced-btns{
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-between;
+            align-items: center;
+        }
+        .animate-spin {
+            animation: spin 1s linear infinite;
+        }
+        @keyframes spin {
+            from {
+                transform: rotate(0deg);
+            }
+            to {
+                transform: rotate(360deg);
+            }
+        }
+        #share-btn-container {
+            display: flex; padding-left: 0.5rem !important; padding-right: 0.5rem !important; background-color: #000000; justify-content: center; align-items: center; border-radius: 9999px !important; width: 13rem;
+        }
+        #share-btn {
+            all: initial; color: #ffffff;font-weight: 600; cursor:pointer; font-family: 'IBM Plex Sans', sans-serif; margin-left: 0.5rem !important; padding-top: 0.25rem !important; padding-bottom: 0.25rem !important;
+        }
+        #share-btn * {
+            all: unset;
+        }
+        .gr-form{
+            flex: 1 1 50%; border-top-right-radius: 0; border-bottom-right-radius: 0;
+        }
+        #prompt-container{
+            gap: 0;
+        }
+        #generated_id{
+            min-height: 700px
+        }
+"""
+block = gr.Blocks(css=css)
+
+examples = [
+   
+    [
+        'Thinking man in anime style'
+    ],
+   
+]
+
+with block as demo:
+    gr.Markdown("""
+       
+
+[![Framework: PyTorch](https://img.shields.io/badge/Framework-PyTorch-orange.svg)](https://pytorch.org/) [![Huggingface space](https://img.shields.io/badge/🤗-Huggingface-yello.svg)](https://huggingface.co/sberbank-ai/Kandinsky_2.0)
+[Offical BlogPost](https://habr.com/ru/company/sberbank/blog/725282/)
+
+## Model architecture:
+Kandinsky 2.1 inherits best practicies from Dall-E 2 and Latent diffucion, while introducing some new ideas.
+
+As text and image encoder it uses CLIP model and diffusion image prior (mapping) between latent spaces of CLIP modalities. This approach increases the visual performance of the model and unveils new horizons in blending images and text-guided image manipulation.
+
+For diffusion mapping of latent spaces we use transformer with num_layers=20, num_heads=32 and hidden_size=2048.
+
+Other architecture parts:
+
+- Text encoder (XLM-Roberta-Large-Vit-L-14) - 560M
+- Diffusion Image Prior — 1B
+- CLIP image encoder (ViT-L/14) - 427M
+- Latent Diffusion U-Net - 1.22B
+- MoVQ encoder/decoder - 67M
+
+Kandinsky 2.1 was trained on a large-scale image-text dataset LAION HighRes and fine-tuned on our internal datasets.
+
+**Kandinsky 2.0** architecture overview:
+![](kandi2.png)
+             
+        """
+    )
+    with gr.Group():
+        with gr.Box():
+            with gr.Row().style(mobile_collapse=False, equal_height=True):
+
+                text = gr.Textbox(
+                    label="Enter your prompt", show_label=False, max_lines=1
+                ).style(
+                    border=(True, False, True, True),
+                    rounded=(True, False, False, True),
+                    container=False,
+                )
+                btn = gr.Button("Run").style(
+                    margin=False,
+                    rounded=(False, True, True, False),
+                )
+               
+        gallery = gr.Gallery(label="Generated images", show_label=False, elem_id="generated_id").style(
+            grid=[2], height="auto"
+        )
+        
+        ex = gr.Examples(examples=examples, fn=infer, inputs=[text], outputs=gallery, cache_examples=True)
+        ex.dataset.headers = [""]
+        
+        text.submit(infer, inputs=[text], outputs=gallery)
+        btn.click(infer, inputs=[text], outputs=gallery)
+gr.Markdown("""
+    
+    
+# Authors
+
++ Arseniy Shakhmatov: [Github](https://github.com/cene555), [Blog](https://t.me/gradientdip)
++ Anton Razzhigaev: [Github](https://github.com/razzant), [Blog](https://t.me/abstractDL)
++ Aleksandr Nikolich: [Github](https://github.com/AlexWortega), [Blog](https://t.me/lovedeathtransformers)
++ Vladimir Arkhipkin: [Github](https://github.com/oriBetelgeuse)
++ Igor Pavlov: [Github](https://github.com/boomb0om)
++ Andrey Kuznetsov: [Github](https://github.com/kuznetsoffandrey)
++ Denis Dimitrov: [Github](https://github.com/denndimitrov)
+
+    """
+    )
+    
+demo.queue(max_size=2).launch()
